874cb937a2c605d8fa6cc0dc7fd1d459dbc22fd1
safal-t <safaltimalsena0@gmail.com>
2025-06-18 01:58:52 +1000

    chore: rearrange files

--- src/js/dom/dom.js
+++ src/js/dom/dom.js
@@ -10,4 +10,9 @@ function initializeBoard(board, player) {
     });
 }
 
-export default initializeBoard
+function updateCell(cell, typeOfUpdate) {
+    const square = document.querySelector(`[data-coordinates="${cell.x}, ${cell.y}"]`)
+    square.classList.add(`${typeOfUpdate}`)
+}
+
+export { initializeBoard, updateCell }
