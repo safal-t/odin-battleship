37f452f1d905a38c792e533fd3f2961441db78af
safal-t <safaltimalsena0@gmail.com>
2025-06-20 22:20:49 +1000

    feat(GameController): create scaffold (define methods) for GameController and implement startGame method

--- src/js/logic/GameController.js
+++ src/js/logic/GameController.js
@@ -1,2 +1,36 @@
 import Player from './Player/Player.js'
 import { initializeBoard, updateCell } from '../dom/dom.js'
+
+function GameController() {
+
+    const humanPlayer = Player("player")
+    const computerPlayer = Player("computer")
+    const currentPlayer = humanPlayer
+
+    function startGame() {
+        initializeBoard(humanPlayer.getGameBoard.getBoard, humanPlayer)
+        initializeBoard(computerPlayer.getGameBoard.getBoard, computerPlayer)
+    }
+
+    function endGame() {
+
+    }
+
+    function initializeGame() {
+        // do i need?? 
+    }
+
+    function manageTurn(currentPlayer) {
+
+    }
+
+    function receiveAttack() {
+
+    }
+
+    return {
+        startGame
+    }
+}
+
+export default GameController
