a563a55b943aa7803115a68ce500a8db6e280e51
safal-t <safaltimalsena0@gmail.com>
2025-06-25 22:38:44 +1000

    feat(Player): implement randomMove method

--- src/js/logic/Player/Player.js
+++ src/js/logic/Player/Player.js
@@ -2,6 +2,22 @@ import Gameboard from '../Gameboard/Gameboard.js'
 
 function Player(type) {
     const gameBoard = Gameboard(type)
+
+    const movesMade = new Set()
+
+    function makeRandomMove() {
+        while (true) {
+            const x = Math.floor(Math.random() * 10)
+            const y = Math.floor(Math.random() * 10)
+            const coord = `${x},${y}`
+
+            if (!movesMade.has(coord)) {
+                movesMade.add(coord)
+                return { x, y }
+            }
+        }
+    }
+
     return {
         type,
         get getGameBoard() {
